3f7386ceea296fb1b6968ad18988b38a0eaadbba
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-17 05:18:19 +0100

    role conditional access working

--- src/main/java/com/example/restapi2/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/restapi2/configuration/SecurityConfiguration.java
@@ -5,28 +5,43 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import jakarta.servlet.DispatcherType;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    // Virtual userDetailsService
+    @Bean
+    public InMemoryUserDetailsManager user() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("ced").password("{noop}ced123").authorities("read").roles("ADMIN").build());
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
                 .authorizeHttpRequests(authorize -> {
-                    authorize.dispatcherTypeMatchers(DispatcherType.ERROR, DispatcherType.FORWARD).permitAll()
+                    authorize
+                            // .dispatcherTypeMatchers(DispatcherType.ERROR,
+                            // DispatcherType.FORWARD).permitAll()
                             .requestMatchers(new AntPathRequestMatcher("/users**")).permitAll()
                             .requestMatchers(new AntPathRequestMatcher("/users**", "GET")).permitAll()
-                            .requestMatchers(new AntPathRequestMatcher("/test**")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/test1**")).hasRole("ADMIN")
+                            .requestMatchers(new AntPathRequestMatcher("/test2**")).hasRole("USER")
                             .requestMatchers(new AntPathRequestMatcher("/h2**")).permitAll()
                             .anyRequest().authenticated();
                     // .anyRequest().permitAll();
-                }).formLogin(Customizer.withDefaults()).build();
+                }).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // basic popup login form
+                .httpBasic(Customizer.withDefaults()).build();
+        // html login
+        // .formLogin(Customizer.withDefaults()).build();
     }
 }
 
