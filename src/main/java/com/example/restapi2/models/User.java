4c2c8985d11eacfff0c7a796a2ac602cd5fa2626
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-20 18:52:30 +0100

    updated role model

--- src/main/java/com/example/restapi2/models/User.java
+++ src/main/java/com/example/restapi2/models/User.java
@@ -1,19 +1,21 @@
 package com.example.restapi2.models;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -51,14 +53,17 @@ public class User implements UserDetails {
 
     // !!! https://www.baeldung.com/jpa-no-argument-constructor-entity-class
     public User() {
+        super();
+        authorities = new HashSet<>();
     }
 
-    public User(String firstName, String lastName, String email, String password, Role role) {
+    public User(String firstName, String lastName, String email, String password, Set<Role> authorities) {
+        super();
         this.firstname = firstName;
         this.lastname = lastName;
         this.email = email;
         this.password = password;
-        this.role = role; // if role is not defined can't pass through spring security
+        this.authorities = authorities;
     }
 
     @Enumerated(EnumType.STRING)
@@ -66,14 +71,21 @@ public class User implements UserDetails {
 
     // methods that needs to be overriden to implements userDetails
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        String userRole = role.name();
-        if (userRole.contains("ROLE_") == false) {
-            userRole = "ROLE_" + userRole;
-        }
-        return List.of(new SimpleGrantedAuthority(userRole));
-    }
+    /*
+     * @Override
+     * public Collection<? extends GrantedAuthority> getAuthorities() {
+     * String userRole = role.name();
+     * if (userRole.contains("ROLE_") == false) {
+     * userRole = "ROLE_" + userRole;
+     * }
+     * return List.of(new SimpleGrantedAuthority(userRole));
+     * }
+     */
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role_junction", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
+            @JoinColumn(name = "role_id") })
+    private Set<Role> authorities;
 
     @Override
     public String getUsername() { // identifiant login
